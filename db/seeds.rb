e67aacb479f67e35d438c963808d0c04713f05db
neil patel <neil@myindianweddingapp.com>
2016-12-04 15:25:58 +0000

    Added index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+puts "1 User Created"
+
 100.times do |post|
-  Post.create!(date: Date.today, description: "#{post} description content")
+  Post.create!(date: Date.today, description: "#{post} description content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
