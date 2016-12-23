a9d54e0967af5df6712ff3d5f687f04ba24e2693
neil patel <neil@myindianweddingapp.com>
2016-12-23 19:25:32 +0000

    Created audit log resource and relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :posts
+  has_many :audit_logs
   validates_presence_of :first_name, :last_name, :phone
 
   PHONE_REGEX = /\A[0-9]*\Z/
