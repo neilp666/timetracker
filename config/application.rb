73d5d20e0c585cd397aa5ccec79aa038b5398405
neil patel <neil@myindianweddingapp.com>
2016-12-24 17:36:45 +0000

    Implemented audit log view

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ Bundler.require(*Rails.groups)
 module Timetracker
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
-   config.active_record.raise_in_transactional_callbacks = true
-   config.autoload_paths << Rails.root.join("lib")
+    config.autoload_paths << Rails.root.join("lib")
   end
 end
