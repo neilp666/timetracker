6e2117033599c289ceaad767372feabbdd0028fe
neil patel <neil@myindianweddingapp.com>
2016-12-27 11:27:32 +0000

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,15 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin User created"
 
-100.times do |post|
-  Post.create!(date: Date.today, description: "#{post} description content goes here, description content goes here, description content goes here", user_id: @user.id, overtime_request: 2.5)
-end
-
-puts "100 Posts have been created"
 
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
+
+100.times do |post|
+  Post.create!(date: Date.today, description: "#{post} description content goes here, description content goes here, description content goes here", user_id: @user.id, overtime_request: 2.5)
+end
+
+puts "100 Posts have been created"
